84a9dde3fe5226e4317b11aed4931e9be890abb7
VSDEVELOPERS-UAE <vishnus4ji@gmail.com>
2025-07-31 14:14:54 +0400

    Delete AssetoriX/backup.py

--- AssetoriX/backup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("db.sqlite3")
-with open("backup.sql", "w") as f:
-    for line in con.iterdump():
-        f.write('%s\n' % line)
-con.close()
-print("✅ Database dumped to backup.sql")
